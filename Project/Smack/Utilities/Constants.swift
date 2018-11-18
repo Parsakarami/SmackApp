5a72834d1050f92994dd4cf5b538285c6f28c2db
Parsa <parsa@Parsas-Mac.local>
2018-11-17 20:39:28 -0800

    add Auth Service

--- Project/Smack/Utilities/Constants.swift
+++ Project/Smack/Utilities/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//Service URL constants
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//Segues
 let To_Login = "loginSegue"
 let To_Create_Account = "registerSegue"
 let UNWIND = "unWindToChannel"
+
+//Auth
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
